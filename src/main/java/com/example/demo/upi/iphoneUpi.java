2eff0c66f5c6502148790edde3e25bbbbead292e
Viet Anh <anhviet24102001@gmail.com>
2022-03-21 16:33:26 +0700

    Add mierge

--- src/main/java/com/example/demo/upi/iphoneUpi.java
+++ src/main/java/com/example/demo/upi/iphoneUpi.java
@@ -4,5 +4,8 @@ public class iphoneUpi {
     public void iphone(){
         System.out.println("iphone 13 pro max");
         System.out.println("iphone 12 pro max");
+
+        System.out.println("iphone 13 pro max");
+        System.out.println("iphone 12 pro max");
     }
 }
