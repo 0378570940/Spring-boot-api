74187e03225f0993e87e4662203ef86a2f4ddb02
Viet Anh <anhviet24102001@gmail.com>
2022-03-21 16:50:01 +0700

    Add iphone

--- src/main/java/com/example/demo/upi/iphone1.java
+++ src/main/java/com/example/demo/upi/iphone1.java
@@ -4,6 +4,16 @@ public class iphone1 extends iphoneUpi {
     public void iphone(){
         System.out.println("iphone 13 pro max");
         System.out.println("iphone 12 pro max");
-        
+
+
+        System.out.println("iphone 13 pro max");
+        System.out.println("iphone 12 pro max");
+
+        System.out.println("iphone 13 pro max");
+        System.out.println("iphone 12 pro max");
+
+        System.out.println("iphone 13 pro max");
+        System.out.println("iphone 12 pro max");
+
     }
 }
