1d32d6e0f9cdb18623756ea396c5a0461de477f3
Viet Anh <anhviet24102001@gmail.com>
2022-03-21 14:51:23 +0700

    Add iphone vs iphone 1

---
+++ src/main/java/com/example/demo/upi/iphone1.java
@@ -0,0 +1,6 @@
+package com.example.demo.upi;
+
+public class iphone1 extends iphoneUpi {
+    public void iphone(){
+    }
+}
---
+++ src/main/java/com/example/demo/upi/iphoneUpi.java
@@ -0,0 +1,8 @@
+package com.example.demo.upi;
+
+public class iphoneUpi {
+    public void iphone(){
+        System.out.println("iphone 13 pro max");
+        System.out.println("iphone 12 pro max");
+    }
+}
