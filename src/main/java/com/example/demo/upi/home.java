cef6118d20435de672d54e009b88cd4d46766db4
Viet Anh <anhviet24102001@gmail.com>
2022-03-21 15:33:09 +0700

    Add home

--- src/main/java/com/example/demo/upi/home.java
+++ src/main/java/com/example/demo/upi/home.java
@@ -1,4 +1,9 @@
 package com.example.demo.upi;
 
 public class home extends iphoneUpi{
+    public void home(){
+        System.out.println("trang chủ đẹp");
+        System.out.println("trang chưa đẹp");
+
+    }
 }
