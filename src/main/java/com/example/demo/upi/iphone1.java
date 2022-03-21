5cb0c579e5cc9bf28a37b39b729c926801648f32
Viet Anh <anhviet24102001@gmail.com>
2022-03-21 16:44:07 +0700

    All home

--- src/main/java/com/example/demo/upi/iphone1.java
+++ src/main/java/com/example/demo/upi/iphone1.java
@@ -4,6 +4,6 @@ public class iphone1 extends iphoneUpi {
     public void iphone(){
         System.out.println("iphone 13 pro max");
         System.out.println("iphone 12 pro max");
-        
+
     }
 }
